85cb594f1c150832e22388ceb2e7972166def97e
Yassine Hemissi <88561527+yassinehemissi@users.noreply.github.com>
2022-07-20 16:21:18 +0100

    Init Commit

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import crypto from "crypto";
 
 let images = {};
@@ -10,7 +9,7 @@ export default function handler(req, res) {
     if (req.body.retrieve)
       return res.status(200).send({ image: images[req.body.retrieve] });
     let code = crypto.randomBytes(10).toString("hex");
-    images[code] = req.body.data;
+    if (Object.keys(images).length < 10) images[code] = req.body.data;
     res.status(200).send({ code: code });
   }
 }
